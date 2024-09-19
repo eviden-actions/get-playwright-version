fb313bc3ef72e9d2083fb674aa10b1b6de0fcd3c
Michaël Vanderheyden <hello@th3s4mur41.me>
2024-09-19 14:54:53 +0000

    fix: build script to include @actions/core

---
+++ src/main.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+async function run() {
+	let version = '';
+	try {
+		const lockfile = require('./package-lock.json');
+		version = lockfile['packages']['node_modules/@playwright/test'].version;
+
+		console.log(`Found Playwright v${version} in the lockfile`);
+		core.setOutput('playwright-version', `v${version}`);
+	} catch (error) {
+		core.setFailed('No Playwright version found');
+	}
+}
+
+module.exports = {
+	run
+};
