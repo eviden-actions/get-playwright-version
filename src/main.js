24e0ffb7338f9785f30f06320342405db2fb198f
Michaël Vanderheyden <hello@th3s4mur41.me>
2024-09-19 15:03:00 +0000

    fix: update path to lockfile

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const core = require('@actions/core');
 async function run() {
 	let version = '';
 	try {
-		const lockfile = require('./package-lock.json');
+		const lockfile = require('../package-lock.json');
 		version = lockfile['packages']['node_modules/@playwright/test'].version;
 
 		console.log(`Found Playwright v${version} in the lockfile`);
